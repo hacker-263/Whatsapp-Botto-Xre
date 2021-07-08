4fa158cb6562c04168fd4cf1738ac40353307129
shimza4 <45925213+shimza4@users.noreply.github.com>
2021-07-08 19:38:55 +0200

    Update help.ts

--- src/lib/help.ts
+++ src/lib/help.ts
@@ -15,15 +15,41 @@ export const help = (client: Client, command?: string): string => {
         }
         return responses['invalid-command-short'].replace('{C}', command)
     }
-    let base = `🤖 ${client._config.name} Command List 🤖\n\n💡 *Prefix:* ${client._config.prefix}\n\n`
+    let base = `╭────┈ ↷
+┃□│✎┊ *ZeD-Bot*
+┃□│╭────────╯
+┃□││❏OWNER : @Hxcker_263
+┃□││❏NAMA : ${client._config.name}
+┃□││❏Team : Team_263  
+┃□││❏GROUP : *${group.metadata.subject}*
+┃□││❏PREFIX : 「 ${client._config.prefix} 」
+╰─────────---────────┈ ❁ཻུ۪۪⸙͎	
+	
+	
+	║█║▌║█║▌│║▌║▌█║
+	${client._config.name}║█║▌║█║▌│║▌║▌█║  Commands\n\n💡 *Prefix:* ${client._config.prefix}\n\n`
     const cmds = commands as ICommandList
     const cats = Object.keys(cmds)
     for (const cat in cmds) {
         base += `*${Utils.capitalize(cat)}* ${Utils.emojis[cats.indexOf(cat)]}\n\`\`\``
         cmds[cat].forEach((cmd) => {
-            base += `${cmd.command}${cmds[cat][cmds[cat].length - 1] === cmd ? '' : ', '}`
+            base += `${cmd.command}${cmds[cat][cmds[cat].length - 4] === cmd ? '' : ', '}`
         })
         base += '```\n\n'
     }
-    return `${base}📚 Use ${client._config.prefix}help <command_name> to view the full info. \n🔖 _Eg: ${client._config.prefix}help promote_`
+    return `${base}📚 Use ${client._config.prefix}help <command_name> to view the full info. \n🔖 _Eg: ${client._config.prefix}help promote_`\n\n'
+	
+	return `	
+	
+	┃□│✎┊ *Team-263-Bot*
+┃□│╭────────╯   
+┃□││❏*Hxcker_263*
+┃□││❏R3tr0_263
+┃□││❏BL4Z3_263
+╰─────────────────┈ ❁ཻུ۪۪⸙͎
+║█║▌║█║▌│║▌║▌█║
+║█║▌║█║▌│║▌║▌█║
+*_ZIM-D4RK-4RMY*`
+	
+	`
 }
